cd844b6bede8f47a68e1934c1282fd6a9919523e
Meriemmatnoussi <meriem.matnoussi123@gmail.com>
2023-06-06 15:37:09 -0700

    8. To uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def uppercase(s):
+    for c in s:
+        if ord(c) >= 97 and ord(c) <= 122:
+            print(chr(ord(c) - 32), end="")
+        else:
+            print(c, end="")
+    print()
