ffaa3732b517766cb0ecded89d5e97d432c536c2
Meriemmatnoussi <meriem.matnoussi123@gmail.com>
2023-06-06 15:12:52 -0700

    3. When I was having that alphabet soup, I never thought that it would pay off

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
+output = ""
+
 for i in range(97, 123):
     if chr(i) != 'q' and chr(i) != 'e':
-        print("{}".format(chr(i)), end='')
+        output += chr(i)
 
-print()
+print("{}".format(output), end="")
