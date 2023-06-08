c65a6f1b046265bb0df29ac3d4357e311f57d0c6
Meriemmatnoussi <meriem.matnoussi123@gmail.com>
2023-06-08 15:25:26 -0700

    4. Who are you?

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import dis
+import hidden_4
+if __name__ == "__main__":
+    checkid = [checkid for checkid in dir(hidden_4)]
+    save_names = sorted(checkid)
+    for checkid in save_names:
+        if not checkid.startswith('__'):
