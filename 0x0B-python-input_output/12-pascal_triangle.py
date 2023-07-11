7efd44a3015f50211b80cad69b68528b19714fbf
Meriemmatnoussi <meriem.matnoussi123@gmail.com>
2023-07-11 16:11:19 -0700

    task 12

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""This defines a Pascal's Triangle function."""
+
+
+def pascal_triangle(n):
+    """Generate Pascal's Triangle of size n.
+
+    Args:
+        n (int): The size of the Pascal's Triangle.
+
+    Returns:
+        list: A list of lists of integers representing the Pascal's Triangle.
+    """
+    if n <= 0:
+        return []
+
+    triangles = [[1]]
+    while len(triangles) != n:
+        tri = triangles[-1]
+        tmp = [1]
+        for i in range(len(tri) - 1):
+            tmp.append(tri[i] + tri[i + 1])
+        tmp.append(1)
+        triangles.append(tmp)
+    return triangles
