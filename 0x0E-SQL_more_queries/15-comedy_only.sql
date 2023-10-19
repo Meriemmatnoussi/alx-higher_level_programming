2acd6f165a4cd0ea99b27161bb0e93f1277db5ea
meriem matnoussi <meriem.matnoussi123@gmail.com>
2023-10-18 17:54:58 -0700

    trying to be spesific

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,3 +1,4 @@
+-- lists all Comedy shows in the database hbtn_0d_tvshows.
 -- The tv_genres table contains only one record where name = Comedy (but the id can be different)
 SELECT tvs.title
 FROM tv_show_genres AS tvsg
